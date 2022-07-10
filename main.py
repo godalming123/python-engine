a7f7eb4782dc9d35ec521fce4c264782bd250c30
godalming123 <why@gmail.com>
2022-07-10 11:36:44 +0100

    finally! I can make 2d lines!

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-#import numpy as num
 import itertools
 import os
 import time
-
-class piont:
-    def __init__ (self,x,y):
-        self.x = x
-        self.y = y
+import math
 
 class screen:
     def __init__ (self,width,height):
@@ -17,10 +12,39 @@ class screen:
     def setPix(self,X,Y,newChar="█"):
       if 0 <= X < self.width and 0 <= Y < self.height: # If co-ordinates are within our screens hight and width
           # Then set the charecter on the co-ordinate should be updated
-          X *= 2                                                                  # double the X becuase 2 chareters is one pixel
+          X *= 2 # double the X becuase 2 chareters is one pixel
+          #X = int(X)
           self.contents[Y] = self.contents[Y][0:X] + (newChar * 2) + self.contents[Y][X+2:] # update the pixel
       else:
           raise ValueError("X and Y must be above or equal to zero and below the width/height of the screen")
+
+    def drawLine(self,Ax,Ay,Bx,By,lineChar="█"):
+      incX = math.copysign(1,Bx-Ax)
+      incY = math.copysign(1,By-Ay)
+
+      dX   =      abs(Bx-Ax)
+      dY   =      abs(By-Ay)
+  
+      XaY  = dX > dY
+      cmpt = max(dX,dY)
+      incD = -2*abs(dX-dY)
+      incS = 2*min(dX,dY)
+
+      err = incD + cmpt
+      X = Ax
+      Y = Ay
+
+      while cmpt >= 0:
+          self.setPix(int(X),int(Y),lineChar)
+          cmpt -= 1
+          if err >= 0 or XaY:
+              X += incX
+          if err>=0 or not(XaY):
+              Y += incY
+          if err >= 0:
+              err += incD
+          else:
+              err += incS
 
     def printMe(self):
         # = print column headers =
@@ -34,8 +58,8 @@ class screen:
         # = print rows =
         lineNo = 0
         for line in self.contents:
-            lineNo += 1
             print(addWhitespace(str(lineNo),10) + " " + line)
+            lineNo += 1
 
 def addWhitespace(obj, minimumCharecters):
     objLen = len(obj)
@@ -48,41 +72,29 @@ def addWhitespace(obj, minimumCharecters):
 def clearScreen():
     os.system("clear")
 
-#def drawLine(screen, A, B):
-#    incX = num.sign(B.x-A.x)
-#    dX   =      abs(B.x-a.X)
-#
-#    incY = num.sign(B.y-A.y)
-#    dY   =      abs(B.y-A.y)
-#
-#    XaY  = dX > dY
-#    cmpt = max(dX,dY)
-#    incD = -2*abs(dX,dY)
-#    incS = 2*min(dX,dY)
-#
-#    err = incD + cmpt
-#    X = A.x
-#    Y = A.y
-#
-#    while cmpt >= 0:
-#        pass
+if __name__ == "__main__":
+    startTime = time.time()
 
-startTime = time.time()
+    screen = screen(32,32)
 
-screen = screen(32,32)
+    #X = 0
+    #Y = 0
 
-X = 0
-Y = 0
+    #while X < screen.width and Y < screen.height:
+    #    clearScreen()
+    #    screen.setPix(X,Y)
+    #    screen.printMe()
+    #    #time.sleep(0.4)
 
-while X < screen.width and Y < screen.height:
-    clearScreen()
-    screen.setPix(X,Y)
+    #    X += 1
+    #    Y += 1
+
+    screen.drawLine(0,0,16,20)
+    screen.drawLine(17,21,25,24,"*")
+    screen.drawLine(25,23,31,6)
+    screen.setPix(0,0)
     screen.printMe()
-    #time.sleep(0.4)
 
-    X += 1
-    Y += 1
-
-print('took: ' + str(time.time() - startTime))
+    print('took: ' + str(time.time() - startTime))
 
 
--- shell.nix
+++ shell.nix
@@ -1,8 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  python-with-my-packages = pkgs.python3.withPackages (p: with p; [
-    #numpi
-    #time
-    #os
-  ]);
+  python-with-my-packages = pkgs.python311.withPackages (p: with p; []);
 in python-with-my-packages.env
